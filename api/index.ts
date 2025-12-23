09ad396cf11eef5990664cda35d6f5153e22acbc
Alexander <18-a.korenev@users.noreply.gitlab.rocketnetwork.site>
2025-12-23 14:13:44 +0100

    Try edge runtime

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+export const config = {
+	runtime: "edge",
+};
+
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello from Vercel!"));
